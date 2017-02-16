394b0ebdf5e78894c976f1ac74577f3e5b390acf
Jeff Hertzler <jeffhertzler@gmail.com>
2017-02-16 07:43:17 -0500

    working, using Agent

--- lib/rpn.ex
+++ lib/rpn.ex
@@ -1,18 +1,32 @@
 defmodule Rpn do
-  @moduledoc """
-  Documentation for Rpn.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Rpn.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def start do
+    Agent.start(fn -> [] end)
   end
+
+  def peek(pid) do
+    Agent.get(pid, fn(list) -> list end)
+  end
+
+  def push(pid, :+) do
+    Agent.update(pid, fn(list) ->
+      [second | [first | tail]] = list
+      [first + second | tail]
+    end)
+  end
+  def push(pid, :-) do
+    Agent.update(pid, fn(list) ->
+      [second | [first | tail]] = list
+      [first - second | tail]
+    end)
+  end
+  def push(pid, :x) do
+    Agent.update(pid, fn(list) ->
+      [second | [first | tail]] = list
+      [first * second | tail]
+    end)
+  end
+  def push(pid, num) do
+    Agent.update(pid, fn(list) -> [num] ++ list end)
+  end
+
 end
